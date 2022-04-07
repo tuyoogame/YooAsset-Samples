5c3e098c0a9bffe02ce80a13d9726129ef7c1a8a
hevinci <hevinci@hotmail.com>
2022-04-07 19:30:33 +0800

    Update demo

--- Assets/GameScript/Runtime/BootScene.cs
+++ Assets/GameScript/Runtime/BootScene.cs
@@ -40,7 +40,7 @@ public class BootScene : MonoBehaviour
 
 	public static string GetHostServerURL()
 	{
-		// 安卓模拟器地址："http://10.0.2.2";
+		//string hostServerIP = "http://10.0.2.2"; //安卓模拟器地址
 		string hostServerIP = "http://127.0.0.1";
 
 		if (Application.platform == RuntimePlatform.Android)
