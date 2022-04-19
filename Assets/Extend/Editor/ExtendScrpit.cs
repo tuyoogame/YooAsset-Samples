b28153bc191910a8d3ba1777854ff0a7f72b6ca1
hevinci <hevinci@hotmail.com>
2022-04-19 10:21:11 +0800

    Update demo

---
+++ Assets/Extend/Editor/ExtendScrpit.cs
@@ -0,0 +1,11 @@
+﻿
+namespace YooAsset.EditorExtend
+{
+	/// <summary>
+	/// 可以访问到YooAsset内部所有的internal类和方法
+	/// </summary>
+	public class ExtendScrpit
+	{
+		private PatchManifest InternalClass;
+	}
+}
